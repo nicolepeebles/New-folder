d9256ef88dbb08bb5f8ca725243d308c44d4e60d
nicolepeebles <68212386+nicolepeebles@users.noreply.github.com>
2020-12-05 16:34:08 -0600

    hw19

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(".GRIT").hover(function(){
+    $(this).css("background-color", "#f8d266");
+    }, function(){
+    $(this).css("background-color", "#FDF4D9");
+  });
+
+$(".FTC").hover(function(){
+    $(this).css("background-color", "#8fb0a9");
+    }, function(){
+    $(this).css("background-color", "#e3ebe9");
+  });
+
+$(".PP").hover(function(){
+    $(this).css("background-color", "#d76735");
+    }, function(){
+    $(this).css("background-color", "#f5d9cd");
+  });
